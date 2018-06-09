e151c4471d3cc0ab0e6b439e1c07dfc0168751b1
infernalslam <infernal-slam@hotmail.com>
2018-06-10 02:29:52 +0700

    init router koa

---
+++ src/controllers/test.controller.js
@@ -0,0 +1,7 @@
+const testController = {
+  async helloWorld (ctx) {
+    ctx.body = 'hello world !'
+  }
+}
+
+module.exports = testController
--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 const Koa = require('koa')
 const app = new Koa()
+const router = require('./controllers')
 
-app.use(async ctx => {
-  ctx.body = 'Hello World'
-})
+app.use(router.routes())
 
-app.listen(3000)
+const env = process.env.NODE_ENV || 'local'
+const port = process.env.PORT || 3000
+app.listen(port)
+console.log('*Environment: ', env)
+console.log('Server is listening @ port ' + port)
+
+exports.default = app
