1874f1791322ec4583d22b51febe9959f4b9dae9
infernalslam <infernal-slam@hotmail.com>
2018-08-04 23:21:52 +0700

    fix code

--- src/middleware/response.js
+++ src/middleware/response.js
@@ -2,7 +2,7 @@ const responseMiddle = async (ctx, next) => {
   try {
     await next()
   } catch (err) {
-    console.log(err)
+    // console.log(err)
     ctx.status = parseInt(err.status, 10)
     ctx.body = err.message
     // ctx.throw(parseInt(err.status, 10), err.message)
